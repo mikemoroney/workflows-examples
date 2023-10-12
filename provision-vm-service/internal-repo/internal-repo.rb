36d24427b85e6803a190165c82f3349b0b0c4009
mikemoroney <141825823+mikemoroney@users.noreply.github.com>
2023-10-12 13:34:44 +0100

    Update internal-repo.rb

--- provision-vm-service/internal-repo/internal-repo.rb
+++ provision-vm-service/internal-repo/internal-repo.rb
@@ -2,18 +2,10 @@
 
 require "manageiq-api-client"
 
-#secrets = JSON.load(File.read(ENV.fetch("SECRETS")))
-
-#user     = secrets.fetch("api_user", "admin")
-#password = secrets.fetch("api_password", "smartvm")
 user = "admin"
 password = "smartvm" 
 
-#url           = ENV.fetch("API_URL", "https://9.37.205.139")
 url = "https://9.37.205.139"
-provider_type = ENV.fetch("PROVIDER_TYPE", nil)
-#verify_ssl    = ENV.fetch("VERIFY_SSL", "true") == "true"
-#verify_ssl = "false"
 
 api = ManageIQ::API::Client.new(
   :url      => url,
@@ -23,7 +15,6 @@ api = ManageIQ::API::Client.new(
 )
 
 response = api.providers
-response = response.where(:type => provider_type) if provider_type
 response = response.pluck(:id, :name)
 
 puts({"values" => response.to_h}.to_json)
