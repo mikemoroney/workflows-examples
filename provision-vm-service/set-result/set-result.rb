952809191a767608a63df9d1f1922e9ab4ab45b7
mikemoroney <141825823+mikemoroney@users.noreply.github.com>
2024-01-05 16:31:31 +0000

    Update set-result.rb

--- provision-vm-service/set-result/set-result.rb
+++ provision-vm-service/set-result/set-result.rb
@@ -1,34 +1,5 @@
 #!/usr/bin/env ruby
 
-require "manageiq-api-client"
-
-secrets = JSON.load(File.read(ENV.fetch("_CREDENTIALS")))
-
-user     = secrets.fetch("api_user", "adminxxx")
-password = secrets.fetch("api_password", "smartvmxxx")
-url = secrets.fetch("url", "https://9.37.205.111")
-#user = "admin"
-#password = "smartvm" 
-
-#url           = ENV.fetch("API_URL", "https://9.37.205.139")
-#url = "https://9.37.205.139"
-provider_type = ENV.fetch("PROVIDER_TYPE", nil)
-#verify_ssl    = ENV.fetch("VERIFY_SSL", "true") == "true"
-#verify_ssl = "false"
-
-api = ManageIQ::API::Client.new(
-  :url      => url,
-  :user     => user,
-  :password => password,
-  :ssl      => {:verify => false}
-)
-
-response = api.providers
-response = response.where(:type => provider_type) if provider_type
-response = response.pluck(:id, :name)
-
-#puts({"values" => response.to_h}.to_json)
-
 result = { 
   token => "abc123"
 } 
