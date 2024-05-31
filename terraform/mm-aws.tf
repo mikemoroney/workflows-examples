3c2d4d5d7beb0609f20aaece6d785569dffa7470
mikemoroney <141825823+mikemoroney@users.noreply.github.com>
2024-05-31 12:24:21 +0100

    Update mm-aws.tf

--- terraform/mm-aws.tf
+++ terraform/mm-aws.tf
@@ -1,1 +1,14 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
 
+# Create an EC2 instance
+resource "aws_instance" "mmtf1" {
+  ami           = "ami-0d1376ce0abc23950"
+  instance_type = "t2.large"
+  
+  tags {
+    Name = "terraform-example"
+  }
+}
